10a6846fe288802945ac3c00f44f81168d4f82e2
Unknown <e.z.l@live.com>
2018-03-18 15:36:11 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,14 +1,11 @@
-document.getElementById("getPersonById").onclick = getAllPersons;
-//document.getElementById("hobbyButton").onclick = ....
-//document.getElementById("cityButton").onclick = ....
-//document.getElementById("hobbyCountButton").onclick = ....
-//document.getElementById("AllZipsButton").onclick = ....
-//document.getElementById("hobbyButton").onclick = ....
+document.getElementById("getPersonById").onclick = getPersonById;
+document.getElementById("getAllPersons").onclick = getAllPersons;
+document.getElementById("getContactById").onclick = getContactInfoById
 
-function getAllPersons()
+function getPersonById()
 {
     var number = document.getElementById("personID").value;
-    var url = "https://localhost:8084/CA2/api/person/complete/" + number;
+    var url = "http://localhost:8084/CA2/api/person/complete/" + number;
     fetch(url)
     .then(result => result.json())
     .then(function(data)
@@ -16,8 +13,9 @@ function getAllPersons()
         var table = "<table>";
         table += "<tr><th>Id</th><th>FirstName</th><th>LastName</th><th>Email</th><th>Address</th><th>Phone</th><th>Hobby</th></tr>";
         table += "<tr>";
-        table += `<td>${data.Fname}</td>`;
-        table += `<td>${data.Lname}</td>`;
+        table += `<td>${data.id}</td>`;
+        table += `<td>${data.fName}</td>`;
+        table += `<td>${data.lName}</td>`;
         table += `<td>${data.email}</td>`;
         table += "<td>";
         table += data.address.street + " ";
@@ -26,10 +24,10 @@ function getAllPersons()
         table += data.address.cityInfoMessage.city;
         table += "</td>";
         table += "<td>";
-        for(var i = 0; i < data.PhoneNumbers.length; i++)
+        for(var i = 0; i < data.phoneNumbers.length; i++)
         {
-            table += data.PhoneNumbers[i].number + " - " + data.PhoneNumbers[i].description;
-            if(i != data.PhoneNumbers.length)
+            table += data.phoneNumbers[i].number + " - " + data.phoneNumbers[i].description;
+            if(i != data.phoneNumbers.length)
                 table += "/";
         }
         table += "</td>";
@@ -40,6 +38,93 @@ function getAllPersons()
             table += data.hobbys[i].description + " ";
         }
         table += "</td>";
+        table += "</tr>";
+        table += "</table>";
+        document.getElementById("content").innerHTML = table;
+        console.log(data);
+    });
+}
+
+function getAllPersons()
+{
+    var url = "http://localhost:8084/CA2/api/person/complete";
+    fetch(url)
+    .then(result => result.json())
+    .then(function(data)
+    {
+        console.log("2");
+        console.log(data);
+        var table = "<table>";
+        table += "<tr><th>Id</th><th>FirstName</th><th>LastName</th><th>Email</th><th>Address</th><th>Phone</th><th>Hobby</th></tr>";
+        for(var i = 0; i < data.length; i++) {
+            table += "<tr>";
+            table += `<td>${data[i].id}</td>`;
+            table += `<td>${data[i].fName}</td>`;
+            table += `<td>${data[i].lName}</td>`;
+            table += `<td>${data[i].email}</td>`;
+            table += "<td>";
+            table += data[i].address.street + " ";
+            table += data[i].address.additionalInfo + " ";
+            table += data[i].address.cityInfoMessage.zip + " ";
+            table += data[i].address.cityInfoMessage.city;
+            table += "</td>";
+            table += "<td>";
+            for (var i = 0; i < data[i].phoneNumbers.length; i++) {
+                table += data[i].phoneNumbers[i].number + " - " + data[i].phoneNumbers[i].description;
+                if (i != data[i].phoneNumbers.length)
+                    table += "/";
+            }
+            table += "</td>";
+            table += "<td>";
+            for (var i = 0; i < data[i].hobbys.length; i++) {
+                table += data[i].hobbys[i].name + " ";
+                table += data[i].hobbys[i].description + " ";
+            }
+            table += "</td>";
+            table += "</tr>";
+        }
+        table += "</table>";
+        document.getElementById("content").innerHTML = table;
+        console.log(data);
+    });
+}
+
+function getContactInfoById()
+{
+    var number = document.getElementById("personIDContact").value;
+    var url = "http://localhost:8084/CA2/api/person/contactinfo/"+number; 
+    fetch(url)
+    .then(result => result.json())
+    .then(function(data)
+    {
+        var table = "<table>";
+        table += "<tr>";
+        table += "<th>Firstname</th>";
+        table += "<th>Lastname</th>";
+        table += "<th>Email</th>";
+        table += "<th>Phone</th>";
+        table += "<th>Street</th>";
+        table += "<th>AdditionalInfo</th>";
+        table += "<th>Zip</th>";
+        table += "<th>City</th>";
+        table += "</tr>";
+
+        table += "<tr>";
+        table += `<td>${data.fName}</td>`;
+        table += `<td>${data.lName}</td>`;
+        table += `<td>${data.email}</td>`;
+        table += "<td>";
+        for(var i = 0; i < data.phoneList.length; i++)
+        {
+            table += data.phoneList[i].number + " - " + data.phoneList[i].description;
+            if(i != data.phoneList.length)
+                table += "/";
+        }
+        table += "</td>";
+        table += `<td>${data.street}</td>`;
+        table += `<td>${data.additionalInfo}</td>`;
+        table += `<td>${data.zip}</td>`;
+        table += `<td>${data.city}</td>`;
         table += "</tr>";
         table += "</table>";
         document.getElementById("content").innerHTML = table;
